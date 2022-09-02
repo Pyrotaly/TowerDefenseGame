f2b953a5ded632a8d2eb106f9d562fb34c597923
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2022-09-01 21:33:55 -0400

    updating

--- TowerDefenseGame/TowerDefense/Assets/Scripts/Manager/InventoryManager.cs
+++ TowerDefenseGame/TowerDefense/Assets/Scripts/Manager/InventoryManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ededa35ebe8cb78ff54e215ea8f9d2591d43618af6835b19f63ed41b284e174
-size 3363
+oid sha256:b282d5422fd88a37810a11b980433e323e3d1c644e070e96dc0200b0359dfe12
+size 3307
