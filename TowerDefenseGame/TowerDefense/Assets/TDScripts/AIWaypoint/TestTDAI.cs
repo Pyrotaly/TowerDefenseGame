519e4eb54b93b228c1be3cd7dc1b51bf09abcb25
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2022-09-02 11:28:13 -0400

    Making TD Báics

---
+++ TowerDefenseGame/TowerDefense/Assets/TDScripts/AIWaypoint/TestTDAI.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:db9a7e52fa26a9abc5a440a3701eb9e312c83659e6e4b53a0d6658a5ec53964a
+size 310
---
+++ TowerDefenseGame/TowerDefense/Assets/TDScripts/BaseTower.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0ccfeb724f9bbdfd5d86840d69e407b2bf37db6bc9c40cf515d5d12bce6a9569
+size 515
